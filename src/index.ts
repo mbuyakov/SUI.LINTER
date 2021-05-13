814688c3770dfcbc107e0ae85915494a0f83ba23
el1191 <el1191@woyd.ru>
2021-05-13 23:43:51 +0300

    Add --quiet + format code

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,50 @@
 import {resolve} from "path";
 import {ESLint} from "eslint";
 import {getFiles} from "./utils/fs";
+import yargs from "yargs/yargs";
+import {hideBin} from "yargs/helpers";
 
+const options = yargs(hideBin(process.argv)).argv;
 
 (async function main() {
-    let rootDir = process.cwd();
-    let srcDir = process.env.SRC_DIR;
+  let rootDir = process.cwd();
+  let srcDir = process.env.SRC_DIR;
 
 
-    if (!srcDir) {
-        console.log("SRC_DIR env not specified, use default");
-        srcDir = "src";
+  if (!srcDir) {
+    console.log("SRC_DIR env not specified, use default");
+    srcDir = "src";
+  }
+
+  srcDir = resolve(`${rootDir}/${srcDir}`);
+
+  console.log(`Source directory: ${srcDir}`);
+
+  const files = [];
+  for await (const f of getFiles(srcDir)) {
+    if (f.endsWith(".ts") || f.endsWith(".tsx") || f.endsWith(".js") || f.endsWith(".jsx")) {
+      files.push(f);
     }
+  }
+  console.log(`Total files: ${files.length}`);
+  // const configPath = path.resolve(`${rootDir}/.eslintrc.js`);
+  // const eslintConfig = require(configPath);
+  // console.log(eslintConfig);
+  const eslint = new ESLint({
+    cache: true
+  });
+  let results = await eslint.lintFiles(files);
 
-    srcDir = resolve(`${rootDir}/${srcDir}`);
+  if (options.quiet) {
+    console.log("Quiet mode enabled - filtering out warnings");
+    results = ESLint.getErrorResults(results);
+  }
 
-    console.log(`Source directory: ${srcDir}`);
-
-    const files = [];
-    for await (const f of getFiles(srcDir)) {
-        if(f.endsWith(".ts") || f.endsWith(".tsx") || f.endsWith(".js") || f.endsWith(".jsx")) {
-            files.push(f);
-        }
-    }
-    console.log(`Total files: ${files.length}`);
-    // const configPath = path.resolve(`${rootDir}/.eslintrc.js`);
-    // const eslintConfig = require(configPath);
-    // console.log(eslintConfig);
-    const eslint = new ESLint({
-        cache: true
-    });
-    const results = await eslint.lintFiles(files);
-    const formatter = await eslint.loadFormatter("stylish");
-    const resultText = formatter.format(results);
-    console.log(resultText);
+  const formatter = await eslint.loadFormatter("stylish");
+  const resultText = formatter.format(results);
+  console.log(resultText);
 
 })().catch((error) => {
-    process.exitCode = 1;
-    console.error(error);
+  process.exitCode = 1;
+  console.error(error);
 });
--- src/utils/fs.ts
+++ src/utils/fs.ts
@@ -3,13 +3,13 @@ import {promises} from "fs";
 
 
 export async function* getFiles(dir) {
-    const dirents = await promises.readdir(dir, { withFileTypes: true });
-    for (const dirent of dirents) {
-        const res = resolve(dir, dirent.name);
-        if (dirent.isDirectory()) {
-            yield* getFiles(res);
-        } else {
-            yield res;
-        }
+  const dirents = await promises.readdir(dir, {withFileTypes: true});
+  for (const dirent of dirents) {
+    const res = resolve(dir, dirent.name);
+    if (dirent.isDirectory()) {
+      yield* getFiles(res);
+    } else {
+      yield res;
     }
+  }
 }
