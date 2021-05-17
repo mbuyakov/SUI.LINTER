696cec582cbbe4bca354d61a9f32c23cf25a1c6b
el1191 <el1191@woyd.ru>
2021-05-17 21:10:24 +0300

    Enable strict mode

--- src/utils/fs.ts
+++ src/utils/fs.ts
@@ -2,7 +2,7 @@ import {resolve} from "path";
 import {promises} from "fs";
 
 
-export async function* getFiles(dir) {
+export async function* getFiles(dir: string): AsyncGenerator<string> {
   const dirents = await promises.readdir(dir, {withFileTypes: true});
   for (const dirent of dirents) {
     const res = resolve(dir, dirent.name);
