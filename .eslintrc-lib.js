dd1b4e6e41501a87a9f9955a925bc3172f676972
el1191 <el1191@woyd.ru>
2021-10-20 19:25:51 +0300

    Restrict all sub paths

--- .eslintrc-lib.js
+++ .eslintrc-lib.js
@@ -3,7 +3,7 @@ const origin = require("@sui/linter/.eslintrc.js");
 const nri = origin.rules['no-restricted-imports'];
 nri[1].patterns = nri[1].patterns.filter(it => !it.startsWith("@sui") && !it.startsWith("!@sui")); // Disable all sui rules
 nri[1].patterns.push("@sui/all"); // And restrict "all" package
-nri[1].patterns.push("@sui/all/*");
+nri[1].patterns.push("@sui/*/*"); // Restrict all sub paths
 
 module.exports = {
   ...origin,
